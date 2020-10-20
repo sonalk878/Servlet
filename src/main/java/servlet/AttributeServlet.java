0378466a94cbe6d07381d827c4a58ef82d9fa0b5
sonalk878 <46801232+sonalk878@users.noreply.github.com>
2020-10-20 19:02:00 -0400

    Update AttributeServlet.java

--- src/main/java/servlet/AttributeServlet.java
+++ src/main/java/servlet/AttributeServlet.java
@@ -31,7 +31,8 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    {
       if ((name != null && name.length() > 0) && (value != null && value.length() > 0) &&(age != null && value.length()>0))
       {
-         session.setAttribute(name, value, age);
+         session.setAttribute(name, value);
+         session.setAttribute(name, age);
       }
 
    }
